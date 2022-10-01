30fe166b05bc887a895afaf43dbe76bf2092b11f
Aryan Gupta <arynn.gupta@gmail.com>
2022-10-01 12:50:44 +0530

    temp changes

--- lib/utils.py
+++ lib/utils.py
@@ -232,6 +232,7 @@ def render_generator_btn(label, api):
 
 
 def stable_dffusion(label):
+    torch.cuda.empty_cache()
     model_id = "CompVis/stable-diffusion-v1-4"
     device = "cuda"
     value = os.getenv("HF_TOKEN")
